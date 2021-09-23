a8133669276c3abdab3de190437cfcc1be5236b8
MarcosTr145 <91250323+MarcosTr145@users.noreply.github.com>
2021-09-23 10:31:08 +0200

    Add files via upload

---
+++ Practica 1/calculadora automatica.ps1
@@ -0,0 +1,31 @@
+﻿Write-Host "◤▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◥"
+Write-Host "|                                         |"
+Write-Host "|            ♪≡Calculadora≡♪              |"
+Write-Host "|                                         |"
+Write-Host "◣▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◢"
+
+Write-Host " 1. Pasa de numero binario a numero decimal"
+Write-Host " 2. Pasa de numero decimal a numero binario"
+Write-Host " 3. Pasa de numero hexadecimal a binario"
+Write-Host " 4. Pasa de numero binario a hexadecimal"
+
+#1
+$binario = Read-Host "Dime un numero binario"
+$valor=[convert]::toint32($binario, 2)
+"el numero en decimal es " + $valor
+
+#2
+$decimal = Read-Host "Dime un numero decimal"
+[Convert]::ToString($decimal, 2)
+
+#3
+$hexa = Read-Host "Dime un numero en hexadecimal"
+[convert]::ToInt32($hexa, 16)
+[convert]::ToString($valor, 2)
+"El numero en binario es " + $valor
+
+#4
+$hexa = Read-Host "Dime un numero en binario"
+$val =[convert]::ToInt32($hexa, 2)
+$res=[convert]::ToString($val, 16)
+"El numero en hexadecimael es " + $res
